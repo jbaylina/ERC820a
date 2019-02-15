36f31cd54f6598788d584193b6ecd84cd08661d3
Jacques Dafflon <jacques@dafflon.tech>
2019-02-15 16:00:49 +0100

    Update vanity-gen
    - remove code copy/paste
    - use solc
    - only print 0x820 addresses
    - dynamic batch size and iterations

--- scripts/vanitygen-info.js
+++ scripts/vanitygen-info.js
@@ -1,14 +1,18 @@
-const EthereumTx = require('ethereumjs-tx');
-const EthereumUtils = require('ethereumjs-util');
+const ethTx = require('ethereumjs-tx');
+const ethUtils = require('ethereumjs-util');
 const rawTransaction = require('../js/rawTransaction');
 
-const offeset = parseInt(process.argv[2]);
+const offset = parseInt(process.argv[2]);
+const batchsize = parseInt(process.argv[3]);
 
-for (let i = 0; i < 16; i++) {
-  const code = '0x' + require(`../tmp/${i}/artifacts/ERC820aRegistry.json`).compilerOutput.evm.bytecode.object;
-  const tx = new EthereumTx({...rawTransaction, data: code});
-  const contractAddr = EthereumUtils.toChecksumAddress(
-    '0x' + EthereumUtils.generateAddress('0x' + tx.getSenderAddress().toString('hex'), 0 ).toString('hex')
+for (let i = 0; i < batchsize; i++) {
+  const code = ('0x' +
+    require(`../tmp/${i}/artifacts/combined.json`).contracts['./contracts/ERC820aRegistry.sol:ERC820aRegistry'].bin);
+  const tx = new ethTx({...rawTransaction, data: code});
+  const contractAddr = ethUtils.toChecksumAddress(
+    ethUtils.generateAddress(tx.getSenderAddress(), ethUtils.toBuffer(0)).toString('hex')
   );
-  console.log(`${offeset + i} -> ${contractAddr}`);
+  if (contractAddr.startsWith('0x820')) {
+    console.log(`${offset + i} -> ${contractAddr}`);
+  }
 }
